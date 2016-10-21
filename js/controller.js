5261614796c856e1eb6b596d4ff43b4c1a50cfa8
Alejandro Cofré <alejandropcl@gmail.com>
2016-10-21 16:57:37 -0300

    data from firebase

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,7 @@
 angular.module("mfa",[])
 	.controller("fcontroller",function($scope,$http){
 		$scope.posts = [];
-		$http.get("https://comprara-284a3.firebaseio.com/")
+		$http.get("https://comprara-284a3.firebaseio.com/usuarios.json?auth=Hd8SKnnXu8W1EZwmCcVk6XiTeKPrtp5qlgUp2CLk")
 		.success(function(data){
 			console.log(data);
 			$scope.posts = data;
