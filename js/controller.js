9e0ee408be8173732aa18624174eeacf662d3277
Alejandro Cofré <alejandropcl@gmail.com>
2016-10-21 23:13:33 -0300

    ingreso de productos a db

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,12 @@
 angular.module("mfa",[])
 	.controller("fcontroller",function($scope,$http){
 		$scope.posts = [];
-		$http.get("https://comprara-284a3.firebaseio.com/usuarios.json?auth=Hd8SKnnXu8W1EZwmCcVk6XiTeKPrtp5qlgUp2CLk")
-		.success(function(data){
-			console.log(data);
-			$scope.posts = data;
-		})
-		.error(function(err){
+		$http.get("https://comprara-284a3.firebaseio.com/productos.json?auth=Hd8SKnnXu8W1EZwmCcVk6XiTeKPrtp5qlgUp2CLk")
+	.success(function(data){
+		console.log(data);
+		$scope.posts = data;
+	})
+	.error(function(err){
 
-		})
-	});
+	})
+});
